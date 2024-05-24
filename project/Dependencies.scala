6f6c47be9c862690bfc3f27ef2451de996d85c30
Thijs Nissen <thijsnissen@gmail.com>
2024-05-24 17:33:10 +0200

    Formatting

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
   )
 
   lazy val app = Seq(
-    "dev.zio" %% "zio" % Version.zio,
-    "dev.zio" %% "zio-json" % Version.zioJson,
+    "dev.zio"                     %% "zio"                   % Version.zio,
+    "dev.zio"                     %% "zio-json"              % Version.zioJson,
     "com.softwaremill.sttp.tapir" %% "tapir-zio-http-server" % Version.tapir
   )
 
